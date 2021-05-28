f0e27e80d447ffc9c8d62b6f0940b66f8c66b927
ajay297 <dangilajay9@gmail.com>
2021-05-28 09:48:22 +0530

    bug fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 const multer = require('multer');
 const Grid = require('gridfs-stream');
 const methodOverride = require('method-override');
-const PORT = process.env.PORT || 5000;
+
 
 const cors = require("cors");
 const mongoURI = 'mongodb+srv://Mohan:Mohan@1804@cluster0.qcwbo.mongodb.net/vegetable?retryWrites=true&w=majority';
@@ -26,7 +26,7 @@ app.use('/users', userRoute);
 app.get('/Add', function (req, res) {
     res.render("MohanAdd.ejs");
 })
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
     console.log("Our sabzi app is running");
-})
+});
